f2c2746859963d934aad7b899f3b31d5aa365366
Avinash Kumar <51404787+Avinash104@users.noreply.github.com>
2023-06-20 17:05:19 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,78 @@
+import "./styles.css";
+
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = (canvas.width = 800);
+const CANVAS_HEIGHT = (canvas.height = 600);
+
+const enemy1 = new Image();
+
+const numOfEnemies = 15;
+const enemyArray = [];
+let gameFrame = 0;
+
+class Enemy1 {
+  constructor(image) {
+    this.image = new Image();
+    this.image.src = require("../Public/Images/enemy2.png");
+    this.spriteWidth = 266;
+    this.spriteHeight = 188;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
+    this.speed = Math.random() * 4 + 1;
+    this.frame = 0;
+    this.flapSpeed = Math.floor(Math.random() * 3 - 1);
+    this.angle = 0;
+    this.curve = Math.random() * 5;
+    this.angleSpeed = Math.random() * 0.05;
+  }
+  update() {
+    this.x -= this.speed;
+    this.y += this.curve * Math.sin(this.angle);
+    this.angle += this.angleSpeed;
+    if (this.x + this.width < 0) this.x = canvas.width;
+    if (gameFrame % this.flapSpeed === 0) {
+      this.frame < 4 ? (this.frame += 1) : (this.frame = 0);
+    }
+  }
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+class Enemy2 {
+  constructor(image) {
+    this.image = new Image();
+    this.image.src = require("../Public/Images/enemy2.png");
+  }
+}
+
+for (let i = 0; i < numOfEnemies; i++) {
+  enemyArray.push(new Enemy1(enemy1));
+}
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  enemyArray.forEach((enemy) => {
+    enemy.update();
+    enemy.draw();
+  });
+  // ctx.drawImage(enemy1, 0, 0, 300, 300, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  gameFrame++;
+  requestAnimationFrame(animate);
+}
+
+animate();
